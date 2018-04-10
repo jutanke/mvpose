077d80534e4808471a43d20bdf0b9e1e52b215b4
Julian <jutanke@googlemail.com>
2018-04-10 18:53:53 +0200

    wip

---
+++ mvpose/algorithm/peaks3d.py
@@ -0,0 +1,8 @@
+"""
+    Peaks3d hold the weighted triangulated peaks from all views.
+    Also, it takes into account from how many views a point is seen
+"""
+import numpy as np
+import numpy.linalg as la
+
+
