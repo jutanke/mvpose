a77f85fde4b1e340591e2718c69f3d69625b1f60
julian.tanke <julian.tanke@fit.fraunhofer.de>
2018-04-13 15:26:26 +0200

    #4 fix bug in meanshift

---
+++ mvpose/algorithm/meanshift.py
@@ -0,0 +1,18 @@
+import numpy as np
+import mvpose.math as mvmath
+from numba import jit, float64
+
+
+@jit([float64[:](float64[:], float64[: ,:], float64)], nopython=True, nogil=True)
+def m(y, Nx, sigma):
+    num = len(Nx)
+    G = mvmath.gauss3d(y[0]-Nx[: ,0], y[1]-Nx[: ,1], y[2]-Nx[: ,2], sigma)
+
+    result = np.zeros((3,))
+    div = 0
+
+    for i in range(num):
+        result += Nx[i ,0:3] * G[i]
+        div += G[i]
+
+    return result /div
