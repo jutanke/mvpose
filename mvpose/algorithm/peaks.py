7f7b87a898a468edba909d7c04ffd700137054be
Julian <jutanke@googlemail.com>
2018-04-16 15:36:38 +0200

    #4 remove dead code

--- mvpose/algorithm/peaks.py
+++ mvpose/algorithm/peaks.py
@@ -84,46 +84,10 @@ class Peaks3D:
         """
         assert other.n_joints == self.n_joints
 
-        #if simple:
         for j in range(self.n_joints):
             if self.data[j] is None:
                 self.data[j] = other.data[j]
             elif other.data[j] is not None:
                 self.data[j] = np.concatenate(
                     [self.data[j], other.data[j]], axis=0)
-        # else:
-        #     for k in range(self.n_joints):
-        #         current_data = self.data[k]
-        #         other_data = other.data[k]
-        #
-        #         if current_data is None:
-        #             self.data[k] = other_data
-        #         elif other_data is not None:
-        #             # TODO this is ~O(n^2) but should be vastly improvable
-        #             # TODO by using better data structures
-        #
-        #             POINTS_for_joint = current_data.copy()
-        #             for j in range(len(other_data)):
-        #                 p_j = other_data[j][0:3]; w_j = other_data[j][3]
-        #                 j_got_merged = False
-        #                 for i in range(len(POINTS_for_joint)):
-        #                     p_i = POINTS_for_joint[i][0:3]; w_i = POINTS_for_joint[i][3]
-        #
-        #                     dist = la.norm(np.array(p_j - p_i))
-        #                     if dist < merge_distance:
-        #                         w_sum = w_i + w_j
-        #                         a_i = w_i/w_sum
-        #
-        #                         v = p_j - p_i
-        #                         p_new = p_i + v * a_i  # as a_i + a_j = 1
-        #                         px,py,pz = p_new
-        #                         POINTS_for_joint[i] = np.array([px,py,pz,w_sum])
-        #                         j_got_merged = True
-        #                         break  # Found a merging point.. SHOULD WE STOP HERE TODO: maybe this is not correct
-        #
-        #                 if not j_got_merged:  # append to end
-        #                     px,py,pz = p_j
-        #                     POINTS_for_joint = np.append(POINTS_for_joint, np.array([[px,py,pz,w_j]]), axis=0)
-        #
-        #
-        #                 self.data[k] = POINTS_for_joint
+
