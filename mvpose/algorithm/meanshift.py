d2b4d43d182e0697c740c29025ea5b4b4b7e2130
Julian <jutanke@googlemail.com>
2018-04-16 15:01:26 +0200

    #4 find all modes using meanshift

--- mvpose/algorithm/meanshift.py
+++ mvpose/algorithm/meanshift.py
@@ -5,6 +5,60 @@ from scipy.spatial import KDTree
 from numba import jit, float64
 
 
+def find_all_modes(X, r, sigma=None, max_iterations=1000):
+    """
+        finds all modes in the data
+    :param X: [ (x,y,z,score), ... ]
+    :param r:
+    :param sigma:
+    :param max_iterations:
+    :return:
+    """
+    if sigma is None:
+        sigma = r
+
+    # calculate the peak from ALL points
+    # TODO check if this can be done probabilistically,
+    # TODO e.g. using the weights of the points as Pr()
+    n = len(X)
+    all_centers = np.zeros((n,4))
+    for i, p in enumerate(X):
+        y = p[0:3]
+        w = p[3]
+        cx,cy,cz = meanshift(y, X,
+                             r=r,
+                             sigma=sigma,
+                             max_iterations=max_iterations)
+        all_centers[i] = [cx,cy,cz,w]
+
+
+
+    # merge centers
+    # TODO: make this more efficient
+    lookup = KDTree(all_centers[:,0:3])
+    allready_handled = set()
+
+    Peaks = []
+
+    for idx, c in enumerate(all_centers):
+        if idx not in allready_handled:
+            N = lookup.query_ball_point(c[0:3], 50)
+            for n in N:
+                allready_handled.add(n)
+            if len(N) > 1:
+                Peaks.append(N)
+
+    Modes = []
+    for peaks in Peaks:
+        N = all_centers[peaks]
+        total_w = np.sum(N[:,3])
+        Pts = N[:, 0:3]
+        W = np.expand_dims(N[:, 3]/total_w, axis=1)
+        Modes.append(np.sum(Pts * W, axis=0))
+
+    return np.array(Modes)
+
+
 def meanshift(y, X, r, sigma=None, max_iterations=1000):
     """
 
