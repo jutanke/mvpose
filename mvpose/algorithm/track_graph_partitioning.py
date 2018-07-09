7676d2c6f03183c90df61dcaf1c67854077c932f
Julian <jutanke@googlemail.com>
2018-07-09 21:45:35 +0200

    wip: dis-entangle the tracking

--- mvpose/algorithm/track_graph_partitioning.py
+++ mvpose/algorithm/track_graph_partitioning.py
@@ -134,6 +134,7 @@ class ValidImageCandidateExtractor:
         self.ImgsB = ImgsB
         self.pairs = pairs
 
+
 # =========================================
 # GraphPartitioningTracker
 # =========================================
